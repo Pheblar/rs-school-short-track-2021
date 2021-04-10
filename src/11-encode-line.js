5b889ceb16e527dbf30d58f2a3b528f3acee1949
Roman Shor <kstarfol@mail.ru>
2021-04-10 18:13:05 +0300

    feat: add solution for task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,30 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arrStr = str.split('');
+  const newArr = [];
+  let newNewArr = [];
+  for (let i = 0; i < arrStr.length; i++) {
+    if (arrStr[i] === arrStr[i + 1]) {
+      newArr.push(arrStr[i]);
+    } else {
+      newArr.push(arrStr[i]);
+      newArr.push(' ');
+    }
+  }
+  const bufer = newArr.join('').split(' ');
+  for (let j = 0; j < bufer.length; j++) {
+    newNewArr.push(bufer[j].length + bufer[j]);
+  }
+  const answer = [];
+  newNewArr = newNewArr.join('').split('');
+  for (let k = 0; k < newNewArr.length; k++) {
+    if (newNewArr[k] !== newNewArr[k + 1] && newNewArr[k] !== '0' && newNewArr[k] !== '1') {
+      answer.push(newNewArr[k]);
+    }
+  }
+  return answer.join('');
 }
 
 module.exports = encodeLine;
